91703683010dfd6dea6ebcec15dd4c981ba7a6b9
tjasaa9 <tjasa9trost9@gmail.com>
2021-08-22 10:54:28 +0200

    Added a second homework

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#9.2 - Ostanek
 choice = int(input("Choose a number between 1-100 "))
 
 for num in range (1, choice+1):
@@ -10,3 +11,11 @@ for num in range (1, choice+1):
         print("Deljivo s 5")
     else:
         print(num)
+
+
+#9.3 - Make string lowercase
+string1 = input("Please enter a string: ")
+string2 = input("Please add another string: ")
+x = (f"{string1} {string2}").lower()
+print(x)
+
